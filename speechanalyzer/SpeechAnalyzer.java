021414121af460a417284981e72033919b9f6fed
casejae <casejae@onid.orst.edu>
2013-08-10 16:43:56 -0700

    Adding speech tester. Needs CMU Sphinx to compile.

---
+++ speechanalyzer/SpeechAnalyzer.java
@@ -0,0 +1,48 @@
+package edu.cmu.sphinx.demo.speechanalyzer;
+
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+
+//Modified HelloWorld For 
+
+public class SpeechAnalyzer{
+
+    public static void main(String[] args) {
+        ConfigurationManager cm;
+
+        if (args.length > 0) {
+            cm = new ConfigurationManager(args[0]);
+        } else {
+            cm = new ConfigurationManager(SpeechAnalyzer.class.getResource("speechanalyzer.config.xml"));
+        }
+
+        Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+        recognizer.allocate();
+
+        // start the microphone or exit if the programm if this is not possible
+        Microphone microphone = (Microphone) cm.lookup("microphone");
+        if (!microphone.startRecording()) {
+            System.out.println("Cannot start microphone.");
+            recognizer.deallocate();
+            System.exit(1);
+        }
+
+        System.out.println("Say: 'One fish, two fish.' OR 'Red fish blue fish' ");
+
+        // loop the recognition until the programm exits.
+        while (true) {
+            System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+
+            Result result = recognizer.recognize();
+
+            if (result != null) {
+                String resultText = result.getBestFinalResultNoFiller();
+                System.out.println("You said: " + resultText + '\n');
+            } else {
+                System.out.println("I can't hear what you said.\n");
+            }
+        }
+    }
+}
